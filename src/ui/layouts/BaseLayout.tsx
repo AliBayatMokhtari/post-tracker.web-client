44c43c08a430eb19118d1ae6a3dc0eeca51d72fc
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2023-05-05 17:17:01 +0430

    remove demo

--- src/ui/layouts/BaseLayout.tsx
+++ src/ui/layouts/BaseLayout.tsx
@@ -1,12 +1,8 @@
 import { Outlet } from "react-router-dom";
-import { useToast } from "../../application/notification/notification-use-case";
 
 const BaseLayout = () => {
-  const toast = useToast();
-
   return (
     <div className="h-screen w-screen bg-slate-200">
-      <button onClick={() => toast.success("Ali BM")}>Click</button>
       <Outlet />
     </div>
   );
